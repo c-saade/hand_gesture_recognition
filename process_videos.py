a2b161b7257e39f567bc426bdb49897ff2569bd8
Camille Saade <camille.saade1@gmail.com>
2023-08-11 15:20:34 +0200

    added a script to transform WLASL_100 dataset into landmarks

---
+++ process_videos.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+import pandas as pd
+
+wlasl = pd.read_csv('data/WSASL_100/WLASL_100.csv', index_col = 0)
+
+path_in = 'data/WSASL_100/videos/'
+path_out = 'data/WSASL_100/landmarks/'
+
+try:
+    os.mkdir(path_out)
+except:
+    pass
+    
+    
+for index, row in wlasl.iterrows():
+    try:
+        os.mkdir(path_out + row['gloss'])
+    except:
+        pass
+        
+    file_in = path_in + row['file']
+    file_out = path_out + row['gloss'] + "/" + row['file'].strip('.mp4') + '.npy'
+
+
+    command = "echo " + "'python3 get_landmarks.py -i " + file_in + " -o " + file_out + " --no-display'"
+    os.system(command)
