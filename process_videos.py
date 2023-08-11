870ff629a2d69dba76f893bef68c874620990e49
Camille Saade <camille.saade1@gmail.com>
2023-08-11 15:35:30 +0200

    added progress indicator to video processing

--- process_videos.py
+++ process_videos.py
@@ -12,15 +12,19 @@ try:
     os.mkdir(path_out)
 except:
     pass
-    
-    
+
+n_files = wlasl.shape[0]
+
+n = 0
 for index, row in wlasl.iterrows():
+    n = n + 1
     try:
         os.mkdir(path_out + row['gloss'])
     except:
         pass
-        
+       
     file_in = path_in + row['file']
     file_out = path_out + row['gloss'] + "/" + row['file'].strip('.mp4') + '.npy'
-
+    
+    print(n/n_files)
     get_landmarks(file_in, file_out, display = False)
