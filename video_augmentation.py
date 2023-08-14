ab6a5f8884c38b6f88873ee00d4645cd676f8c66
Camille Saade <camille.saade1@gmail.com>
2023-08-14 16:16:45 +0200

    added temporal shift to video augmentation

--- video_augmentation.py
+++ video_augmentation.py
@@ -14,18 +14,20 @@ def frame_augmentation(frame, mirror = False, angle = 0, shift = [0, 0, 0]):
     return(out_frame)
     
     
-def video_augmentation(file_in, file_out = None, max_angle = 35, max_shift = 0.2):
+def video_augmentation(file_in, file_out = None, max_angle = 35, max_shift = 0.2, max_time_shift = 0.3):
     # applies random transformations to a video for data augmentation
     # file_in: input file
     # file_out: optional file name for writing transformed video
     # max_anlge: maximal rotation in degrees
     # max shift: maximal shift (in proportion of the image size)
+    # time_shift: by how much time do we shift the video (as a proportion of the number of frames)
     
     # reading file
     cap = cv2.VideoCapture(file_in)
     # extracting resolution
     width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    n_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     
     # drawing random transformations
     
@@ -36,6 +38,9 @@ def video_augmentation(file_in, file_out = None, max_angle = 35, max_shift = 0.2
     # shift
     shift_x = int(np.random.uniform(-max_shift, max_shift)*width)
     shift_y = int(np.random.uniform(-max_shift, max_shift)*height)
+    
+    # time shift:
+    time_shift = int(np.random.uniform(-max_time_shift, max_time_shift) * n_frames)
     
     # creating a list to extract all frames
     frame_list = []
@@ -49,6 +54,13 @@ def video_augmentation(file_in, file_out = None, max_angle = 35, max_shift = 0.2
             break
     cap.release()
     
+    # shifting the frame list in time
+    print(time_shift)
+    if time_shift > 0:
+        frame_list = frame_list[time_shift:]
+    if time_shift < 0:
+        frame_list = frame_list[:time_shift]
+    # augmenting the video
     augmented_frame_list = [frame_augmentation(frame, mirror, angle, [shift_x, shift_y, 0]) for frame in frame_list]
     
     # write out
@@ -56,4 +68,5 @@ def video_augmentation(file_in, file_out = None, max_angle = 35, max_shift = 0.2
         skvideo.io.vwrite(file_out, frame_list)
     
     return(frame_list)
-
+    
+    
