b436c06e09bf353207b2d8add24fa4f504e56af4
Camille Saade <camille.saade1@gmail.com>
2023-08-16 12:00:20 +0200

    video processing now includes data augmentation

--- process_videos.py
+++ process_videos.py
@@ -2,6 +2,9 @@ import cv2
 import os
 import pandas as pd
 from get_landmarks import get_landmarks
+from video_augmentation import *
+
+import gc
 
 wlasl = pd.read_csv('data/WSASL_100/WLASL_100.csv', index_col = 0)
 
@@ -22,9 +25,30 @@ for index, row in wlasl.iterrows():
         os.mkdir(path_out + row['gloss'])
     except:
         pass
-       
+    # extracting landmarks of original video       
     file_in = path_in + row['file']
     file_out = path_out + row['gloss'] + "/" + row['file'].strip('.mp4') + '.npy'
     
-    print(n/n_files)
+    print(file_out)
+    
     get_landmarks(file_in, file_out, display = False)
+    
+    
+    # adding 10 random transformation of the original video for data augmentation
+    for k in range(10):
+        # randomly transform video and saving in a temp file
+        va = video_augmentation(file_in, 'temp.mp4')
+        # updating file out path
+        file_out = path_out + row['gloss'] + "/" + row['file'].strip('.mp4') + '_' + str(k) + '.npy'
+     
+        print(file_out)
+        
+        # getting landmarks from the temp video file
+        get_landmarks('temp.mp4', file_out, display = False)
+        
+        
+    print(n/n_files)
+    
+    gc.collect()
+    
+os.remove('temp.mp4')
--- video_augmentation.py
+++ video_augmentation.py
@@ -62,10 +62,8 @@ def video_augmentation(file_in, file_out = None, max_angle = 35, max_shift_x = 0
     # augmenting the video
     augmented_frame_list = [frame_augmentation(frame, mirror, angle, [shift_y, shift_x, 0]) for frame in frame_list]
     
-    # write out
-    if file_out:
+    try:
         skvideo.io.vwrite(file_out, augmented_frame_list)
-    
-    return(frame_list)
-    
+    except:
+        print('file_out error')
     
